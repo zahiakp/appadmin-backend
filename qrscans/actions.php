10db98ac274a7c1627ef23eeee0cd6a69a60f6b8
FaleluRahman <158285867+FaleluRahman@users.noreply.github.com>
2025-09-26 22:32:16 +0530

    fixed issues

--- qrscans/actions.php
+++ qrscans/actions.php
@@ -1,13 +1,6 @@
-<?php
-header("Access-Control-Allow-Origin: *");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
-header("Access-Control-Allow-Headers: Content-Type, Authorization");
-header("Content-Type: application/json");
 
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    http_response_code(200);
-    exit();
-}
+
+<?php
 
 include '../inc/head.php';
 include '../inc/const.php';
